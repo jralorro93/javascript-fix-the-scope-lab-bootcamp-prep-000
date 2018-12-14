a9d18785283bd78581a97c6283405a656ab51d3d
Juan Alorro <jralorro93@gmail.com>
2018-12-14 21:47:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ var funkyFunction = function outerFunction() {
   }
 }
 
+<<<<<<< HEAD
 var theFunk = funkyFunction()();
 
 console.log(theFunk()());
+=======
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
+var theFunk = funkyFunction();
+theFunk();
+>>>>>>> 3f27259abc35b6e54ed36f7f0988262ddb342b6b
 
